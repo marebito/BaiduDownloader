fb1f37a79c46afe4add474525e3cf6e2c43eb385
lilung.chao <lilung.chao@gmaiil.com>
2018-05-11 00:28:00 +0800

    clean up the code

--- BaiduDownloader/Model/ArtPhotoModel.h
+++ BaiduDownloader/Model/ArtPhotoModel.h
@@ -10,34 +10,54 @@
 
 @interface PhotoSetsModel : NSObject
 
-@property (nonatomic, copy) NSString *setsNo;           // 套图编号
+@property(nonatomic, copy) NSString *setsNo;  // 套图编号
 
-@property (nonatomic, copy) NSString *setsSize;         // 套图尺寸
+@property(nonatomic, copy) NSString *setsSize;  // 套图尺寸
 
-@property (nonatomic, copy) NSString *setsFileSize;     // 套图大小
+@property(nonatomic, copy) NSString *setsFileSize;  // 套图大小
 
-@property (nonatomic, copy) NSString *decompressPswd;   // 解压密码
+@property(nonatomic, copy) NSString *decompressPswd;  // 解压密码
 
-@property (nonatomic, strong) NSArray *previewImages;   // 套图预览
+@property(nonatomic, strong) NSArray *previewImages;  // 套图预览
 
-@property (nonatomic, copy) NSString *link;             // 链接
+@property(nonatomic, copy) NSString *link;  // 链接
 
+/**
+ 根据element返回PhotoSetsModel实例
+
+ @param element element
+ @return 返回PhotoSetsModel实例
+ */
 + (instancetype)modelWithElement:(ONOXMLElement *)element;
 
 @end
 
 @interface ArtPhotoModel : NSObject
 
-@property(nonatomic, copy) NSString *typeName;
+@property(nonatomic, copy) NSString *typeName;  // 分类名
 
-@property(nonatomic, copy) NSString *typeURL;
+@property(nonatomic, copy) NSString *typeURL;  // 分类链接
 
-@property(nonatomic, copy) NSString *title;
+@property(nonatomic, copy) NSString *title;  // 标题
 
-@property(nonatomic, strong) PhotoSetsModel *photoSets;
+@property(nonatomic, strong) PhotoSetsModel *photoSets;  // 写真详情
 
-@property(nonatomic, copy) NSString *time;
+@property(nonatomic, copy) NSString *time;  // 时间
 
+/**
+ 根据element转ArtPhotoModel实例
+
+ @param element element
+ @return 返回ArtPhotoModel实例
+ */
 + (instancetype)modelWithElement:(ONOXMLElement *)element;
+
+/**
+ 获取分类写真
+
+ @param url 分类链接
+ @param complete 完成回调
+ */
+- (void)getPhotosWithURL:(NSString *)url completionHandler:(void (^)(NSArray *artPhotos))complete;
 
 @end
